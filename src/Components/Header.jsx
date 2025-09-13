4dbba35cb361c6b28ed979a8f9ffc9cc48335e8d
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-09-13 14:49:52 +0530

    Header Feature Updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";   // import Link
+import UserLogin from "../UserServices/UserLogin";
 
 function Header() {
     return (
         <div>
             <ul>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
             </ul>
         </div>
-    )
+    );
 }
 
 export default Header;
