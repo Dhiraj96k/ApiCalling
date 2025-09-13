9010eac2a19a8408d742505fac0f4ab6ad50ea49
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-09-13 14:26:40 +0530

    Added Header feature

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
